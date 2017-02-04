f1c42c92dd604a50287b7581759678d3c4f74ea8
anamritraj <anandamritraj904@gmail.com>
2017-02-04 17:15:06 +0530

    Add routes.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,64 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'api'], function()
+{
+	/////////////////
+	// User Routes //
+	/////////////////
+
+	// Authenticate User
+    Route::post('authenticate', 'UserController@authenticate');
+    
+    // Get user
+    Route::get('user', 'UserController@getUser');
+    
+    // Register a new User
+    Route::post('register', 'UserController@register');
+    
+    // Update user Events
+    Route::post('user/events', 'UserController@updateUserEvents');
+
+    /////////////////
+    //Event Routes //
+    /////////////////
+    
+    // Get All events
+    Route::get('event', 'EventController@index');
+
+	// Get event by event_slug
+    Route::get('event/{event_slug}', 'EventController@getEvent');
+
+    // Create a new Event
+    Route::post('event', 'EventController@createEvent');
+
+    // Update Event
+    Route::put('event', 'EventController@updateEvent');
+    
+    // Delete Event
+    // Note to self: Do not allow anyone to delete the events.
+    Route::delete('event/{event_slug}', 'EventController@deleteEvent');
+
+    /////////////////////
+    // Category Routes //
+    /////////////////////
+	
+	// Get All categories
+    Route::get('category', 'CategoryController@index');
+    
+	// Get category by id
+    Route::get('category/{id}', 'CategoryController@getCategory');
+    
+	// Update Category
+    Route::put('category', 'CategoryController@updateCategory');
+    
+    // Create Category
+    Route::post('category', 'CategoryController@createCategory');
+    
+    // Delete Category
+    Route::delete('category/{id}', 'CategoryController@deleteCategory');
+    
+});
+
